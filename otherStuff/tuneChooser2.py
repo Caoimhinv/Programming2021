4ac1b020231fe5263d3c60f9cbe4f65701f0638e
Caoimhinv <caoimhinvallely@gmail.com>
2021-03-15 18:57:30 +0000

    small updates

--- otherStuff/tuneChooser2.py
+++ otherStuff/tuneChooser2.py
@@ -5,9 +5,13 @@
 
 # ref. for choosing from range of rows https://stackoverflow.com/questions/52152365/reading-just-range-of-rows-from-one-csv-file-in-python-using-pandas
 import pandas as pd
+import csv
 
-df = pd.read_csv('tuneDatabase.csv', nrows=31)
+df = pd.read_csv('tuneDatabase.csv', nrows=32)
 
-tuneOfTheDay1 = df.sample(n=5)
+tunesOfTheDay = df.sample(n=5)
 
-print(tuneOfTheDay1)
+print("5 TUNES TO REVISE TODAY:\n", tunesOfTheDay)
+
+recentlyLearnedTunes = pd.read_csv('tuneDatabase.csv', nrows=2, skiprows=30)
+print ("RECENTLY LEARNED TUNES TO PRACTICE:\n", recentlyLearnedTunes)
