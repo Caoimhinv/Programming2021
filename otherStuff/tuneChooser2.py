0232bba3db2e52cd51202622851379c010430dad
Caoimhinv <caoimhinvallely@gmail.com>
2021-03-18 17:21:57 +0000

    new print statement

--- otherStuff/tuneChooser2.py
+++ otherStuff/tuneChooser2.py
@@ -1,16 +1,14 @@
 # this programme picks 5 random sets of tunes for me to practice each day!
 # Author: Caoimhin Vallely
 
-# doesn't seem to recognise pandas in VSC? Works fine in terminal?
-
 # ref. for choosing from range of rows https://stackoverflow.com/questions/52152365/reading-just-range-of-rows-from-one-csv-file-in-python-using-pandas
 import pandas as pd
+import csv
 
-df = pd.read_csv('/Users/caoimhinvallely/Desktop/Programming/Programming2021/otherStuff/tuneDatabase.csv', nrows=32, index_col=0)
-
+df = pd.read_csv('/Users/caoimhinvallely/Desktop/Programming/Programming2021/otherStuff/tuneDatabase.csv', nrows=33, index_col=0, header=None)
 tunesOfTheDay = df.sample(n=5)
-
 print("5 TUNES TO REVISE TODAY:\n", tunesOfTheDay, "\n")
-
-recentlyLearnedTunes = pd.read_csv('/Users/caoimhinvallely/Desktop/Programming/Programming2021/otherStuff/tuneDatabase.csv', nrows=2, skiprows=30, index_col=0)
-print ("RECENTLY LEARNED TUNES TO PRACTICE:\n", recentlyLearnedTunes)
+recentlyLearnedTunes = pd.read_csv('/Users/caoimhinvallely/Desktop/Programming/Programming2021/otherStuff/tuneDatabase.csv', nrows=3, skiprows=31, index_col=0, header=None)
+print ("RECENTLY LEARNED TUNES TO PRACTICE:\n", recentlyLearnedTunes, "\n")
+todaysNewTune = pd.read_csv('/Users/caoimhinvallely/Desktop/Programming/Programming2021/otherStuff/tuneDatabase.csv', nrows=1, skiprows=34, index_col=0, header=None)
+print("NEW TUNE FOR TODAY:\n ", todaysNewTune, "\n")
