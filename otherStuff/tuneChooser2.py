65c4743110e2e3a6dad26c60a1da82c055e69a09
Caoimhinv <caoimhinvallely@gmail.com>
2021-03-28 20:22:30 +0100

    tidy up

--- otherStuff/tuneChooser2.py
+++ otherStuff/tuneChooser2.py
@@ -1,15 +1,28 @@
 # this programme picks 5 random sets of tunes for me to practice each day!
 # Author: Caoimhin Vallely
 
-# ref. for choosing from range of rows https://stackoverflow.com/questions/52152365/reading-just-range-of-rows-from-one-csv-file-in-python-using-pandas
 import pandas as pd
-import csv
 
-current = 31
-df = pd.read_csv('/Users/caoimhinvallely/Desktop/Programming/Programming2021/otherStuff/tuneDatabase.csv', nrows=current+2, index_col=0, header=None)
-tunesOfTheDay = df.sample(n=5)
-print("5 TUNES TO REVISE TODAY:\n", tunesOfTheDay, "\n")
-recentlyLearnedTunes = pd.read_csv('/Users/caoimhinvallely/Desktop/Programming/Programming2021/otherStuff/tuneDatabase.csv', nrows=3, skiprows=current, index_col=0, header=None)
-print ("RECENTLY LEARNED TUNES TO PRACTICE:\n", recentlyLearnedTunes, "\n")
-todaysNewTune = pd.read_csv('/Users/caoimhinvallely/Desktop/Programming/Programming2021/otherStuff/tuneDatabase.csv', nrows=1, skiprows=current+3, index_col=0, header=None)
-print("NEW TUNE FOR TODAY:\n ", todaysNewTune, "\n")
+df = pd.read_csv('/Users/caoimhinvallely/Desktop/Programming/Programming2021/otherStuff/tuneDatabase.csv', index_col=0)
+status = 34
+
+x = input("Will we move on (or stay put)? y/n :")
+if x == 'y':
+    current = status + 1
+    df1 = df[0:current]
+    tunesOfTheDay = df1.sample(n=5)
+    print("5 TUNES TO REVISE TODAY:\n", tunesOfTheDay, "\n")
+    recentlyLearnedTunes = df1[current-3:current]
+    print ("RECENTLY LEARNED TUNES TO PRACTICE:\n", recentlyLearnedTunes, "\n")
+    todaysNewTune = str(df.iloc[current])
+    print("NEW TUNE FOR TODAY:\n ", todaysNewTune, "\n")
+else:
+    df1 = df[0:status]
+    tunesOfTheDay = df1.sample(n=5)
+    print("5 TUNES TO REVISE TODAY:\n", tunesOfTheDay, "\n")
+    recentlyLearnedTunes = df1[status-4:status-1]
+    print ("RECENTLY LEARNED TUNES TO PRACTICE:\n", recentlyLearnedTunes, "\n")
+    last_learnt_tune = str(df.iloc[status-1])
+    print("LAST LEARNT TUNE TO REVISE:\n ", last_learnt_tune)
+
+
