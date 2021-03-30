5648ce7e63a5f089ca56efeb07b8b57eabc846bc
Caoimhinv <caoimhinvallely@gmail.com>
2021-03-30 09:15:13 +0100

    nailed the current tune dilemna

--- otherStuff/tuneChooser2.py
+++ otherStuff/tuneChooser2.py
@@ -4,18 +4,27 @@
 import pandas as pd
 
 df = pd.read_csv('/Users/caoimhinvallely/Desktop/Programming/Programming2021/otherStuff/tuneDatabase.csv', index_col=0)
-status = 34
+# opens file containing current tune
+with open('/Users/caoimhinvallely/Desktop/Programming/Programming2021/otherStuff/tune_status.txt') as f:
+    status = int(f.read())
 
 x = input("Will we move on (or stay put)? y/n :")
 if x == 'y':
+    # moves to the next tune
     current = status + 1
     df1 = df[0:current]
+    # picks a random sample of 5 tunes to practice
     tunesOfTheDay = df1.sample(n=5)
     print("5 TUNES TO REVISE TODAY:\n", tunesOfTheDay, "\n")
+    # displays the last 3 tunes learnt
     recentlyLearnedTunes = df1[current-3:current]
     print ("RECENTLY LEARNED TUNES TO PRACTICE:\n", recentlyLearnedTunes, "\n")
+    # prints todays new tune
     todaysNewTune = str(df.iloc[current])
     print("NEW TUNE FOR TODAY:\n ", todaysNewTune, "\n")
+    # updates the current tune file
+    with open('/Users/caoimhinvallely/Desktop/Programming/Programming2021/otherStuff/tune_status.txt', 'w+') as t:
+       t.write(str(current))
 else:
     df1 = df[0:status]
     tunesOfTheDay = df1.sample(n=5)
